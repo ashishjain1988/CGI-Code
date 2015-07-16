2359c93e7c1f91a8ac02911aa1e21e07b3a72743
Ashish Jain <jain.ashishjain1@gmail.com>
2015-07-16 14:44:23 -0400

    Updated Parsing Code

--- src/main/java/com/cgix/snfsift/convertor/VCFInfoParser.java
+++ src/main/java/com/cgix/snfsift/convertor/VCFInfoParser.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class VCFInfoParser {
 
 	public static void main(String[] args) throws IOException {
-		String vcfFileName,txtFileName,resultFileName;
+		/*String vcfFileName,txtFileName,resultFileName;
 		vcfFileName = txtFileName = resultFileName = null;
 		for(int i=0;i<args.length;i++)
 		{
@@ -33,8 +33,8 @@ public class VCFInfoParser {
 		{
 			System.err.println("Please give valid number of arguments.");
 			System.exit(0);;
-		}
-		BufferedReader br = new BufferedReader(new FileReader(/*"/home/ashish/snpEff/vcftotxt_CLC.txt"*/txtFileName));
+		}*/
+		BufferedReader br = new BufferedReader(new FileReader("/home/ashish/Downloads/ensembl-tools-release-80/scripts/variant_effect_predictor/result.txt"/*txtFileName*/));
 		//ArrayList<String> dataList = new ArrayList<String>(Arrays.asList("Allele","Annotation","Annotation_Impact","Gene_Name","Gene_ID","Feature_Type","Feature_ID","Transcript_BioType","Rank","HGVS.c","HGVS.p","cDNA.pos / cDNA.length","CDS.pos / CDS.length","AA.pos / AA.length","Distance"));
 		Map<String,Integer> summaryDataFieldColumnMapping = new HashMap<String,Integer>();
 		List<Map<Integer,String>> dataFieldList = new ArrayList<Map<Integer,String>>();
@@ -71,7 +71,7 @@ public class VCFInfoParser {
 		}
 		br.close();
 		Map<String, String> infoDescription = new HashMap<String,String>();
-		br = new BufferedReader(new FileReader(/*"/home/ashish/snpEff/CLCBio_SU314.dbNSFP.vcf"*/vcfFileName));
+		br = new BufferedReader(new FileReader("/home/ashish/Downloads/ensembl-tools-release-80/scripts/variant_effect_predictor/CLCBio_SU314.ann.vcf"/*vcfFileName*/));
 		line = br.readLine();
 		while(line != null && !line.equals("#CHROM"))
 		{
@@ -110,20 +110,25 @@ public class VCFInfoParser {
 		{
 			folder.mkdir();
 		}*/
-		PrintWriter pw = new PrintWriter(/*"/home/ashish/snpEff/vcftest_vcf.txt"*/resultFileName+".txt");
+		PrintWriter pw = new PrintWriter("/home/ashish/Downloads/ensembl-tools-release-80/scripts/variant_effect_predictor/vcftest_vcf.txt"/*resultFileName+".txt"*/);
 		//For Header
 		for(String s : headerList)
 		{
+			//System.out.println(s);
 			String desc = infoDescription.get(s);
 			if(desc == null)
 			{
 				pw.print(s+"\t");
 			}else if(desc.contains("|") && !s.equals("GENEINFO"))
 			{
+				System.out.println(s);
 				String l[] = desc.split("\\|");
 				for(int j=0;j<l.length;j++)
 				{
-					pw.print(l[j].trim()+"\t");
+					String header = l[j].trim();
+					header = header.contains("\"")?header.replace("\"", ""):header;
+					//System.out.println(header);
+					pw.print(header+"\t");
 				}
 			}else
 			{
@@ -141,18 +146,23 @@ public class VCFInfoParser {
 				if(desc != null && desc.contains("|") && !s.equals("GENEINFO"))
 				{
 					int length = desc.split("\\|").length;
+					//System.out.println(s);
 					String data1 = dataFields.get(summaryDataFieldColumnMapping.get(s));
-					data1 = data1.replace("(","");
-					data1 = data1.replace(")","");
-					String data[] = data1.split("\\|");
-					for(int j=0;j<length;j++)
+					//System.out.println(data1);
+					if(data1 != null)
 					{
-						if(j>=data.length)
+						data1 = data1.replace("(","");
+						data1 = data1.replace(")","");
+						String data[] = data1.split("\\|");
+						for(int j=0;j<length;j++)
 						{
-							pw.print("\t");
-						}else
-						{
-							pw.print(data[j]+"\t");
+							if(j>=data.length)
+							{
+								pw.print("\t");
+							}else
+							{
+								pw.print(data[j]+"\t");
+							}
 						}
 					}
 				}else
