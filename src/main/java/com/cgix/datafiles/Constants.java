5daa889db15bbaf8bdfb9474dd4555032af6ca37
Ashish Jain <jain.ashishjain1@gmail.com>
2015-07-23 13:56:36 -0400

    Added constants

---
+++ src/main/java/com/cgix/datafiles/Constants.java
@@ -0,0 +1,47 @@
+package com.cgix.datafiles;
+
+public class Constants {
+
+	public static String CENTER = "CGI_Rutherford";
+	public static String NCBI_BUILD = "GRCh37";
+	public static String year = "2015";
+	public static String CBIOPORTAL = "cbioportal";
+	public static String PATIENT_ID = "Patient ID";
+	public static String CANCER_STUDY = "Cancer Study";
+	public static String ASSAY_TYPE = "Assay Type";
+	public static String SAMPLE_ID = "Sample ID";
+	public static String TYPE_OF_CANCER = "Type of cancer";
+	public static String RUN_SUMMARY = "RunSummary";
+	public static String SAMPLE = "Sample";
+	public static String GENE = "Gene";
+	public static String START_COORDINATE = "Coordinate_3";
+	public static String RUN_ID = "Run Id";
+	public static String RUN_DATE = "Run Date";
+	public static String BASE_CHANGE = "HGVSc_43";
+	public static String TUMOR_SAMPLE_BARCODE = "Tumor_Sample_Barcode";
+	public static String CONFIRMATION = "Confirmation";
+	public static String VALIDATION_METHOD = "Validation_Method";
+	public static String VALIDATION_STATUS = "Validation_Status";
+	public static String COMMENTS = "Comments";
+	public static String FINAL_CALL = "FINAL CALL";
+	public static String FUNCTIONAL_IMPACT = "Functional Impact";
+	public static String VARIANT_CONSEQUENCE = "Consequence_29";
+	public static String GENE_NAME = "Gene_0";
+	public static String VARIANT_LENGTH = "Variant Length_4";
+	public static String VARIANT = "Variant_1";
+	public static String READ_DEPTH = "Read Depth_14";
+	public static String ALT_READ_DEPTH = "Alt Read Depth_15";
+	public static String CHR = "Chr_2";
+	public static String VARIANT_TYPE = "Type_5";
+	
+	public static String DBSNP = "dbSNP ID_45";
+	public static String SIFT = "Sift_40";
+	public static String POLYPHEN = "PolyPhen_41";
+	/*public static String COMMENTS = "Comments";
+	public static String FINAL_CALL = "FINAL CALL";
+	public static String FUNCTIONAL_IMPACT = "Functional Impact";
+	public static String VARIANT_TYP = "Consequence_29";
+	public static String GENE_NAME = "Gene_0";*/
+	
+	
+}
